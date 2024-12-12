938c77c4516d0025ea5c4b8ac1dd587f6f6fa244
DtheMakeeeR <danilgol45@mail.ru>
2024-12-12 11:37:48 +0300

    req

--- MyForm.h
+++ MyForm.h
@@ -1,5 +1,9 @@
 #pragma once
-
+#include <string>
+#include <msclr\marshal_cppstd.h>
+#include "..\tStack\TCalc.h"
+#include "..\tStack\TCalc.cpp"
+//a funny comment
 namespace CppWinForm1 {
 
 	using namespace System;
@@ -34,6 +38,10 @@ namespace CppWinForm1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +56,80 @@ namespace CppWinForm1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(180, 111);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(242, 49);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Вычислить";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(296, 74);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(30, 31);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"=";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 16, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(12, 67);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(278, 38);
+			this->textBox1->TabIndex = 2;
+			this->textBox1->Text = L"Введите формулу";
+			this->textBox1->KeyDown += gcnew System::Windows::Forms::KeyEventHandler(this, &MyForm::textBox1_KeyDown);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(638, 309);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+		
+	}
+	private: System::Void textBox1_KeyDown(System::Object^ sender, System::Windows::Forms::KeyEventArgs^ e) {
+		if (e->KeyData == Keys::Enter) {
+			button1_Click(sender, e);
+		}
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		try {
+		TCalc c1;
+		std::string infix;
+		infix = msclr::interop::marshal_as<std::string>(textBox1->Text);
+		c1.setInfix(infix);	
+		label1->Text = "= " + Convert::ToString(c1.Calc());
+		}
+		catch (...) {
+			label1->Text = "error";
+		}
+	}
+};
 }
